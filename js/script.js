92684d08007cc02236b49bc9e48feee98bb376da
Brett Jenaway <w0104215@umail.usq.edu.au>
2026-05-08 10:30:55 +0800

    Initial outline of hero image and webpage

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/* 
+    University of Southern Queensland
+    CSC5740 Client-Side Web Technology
+    Trimester 1, 2026
+
+    Assignment 4: Project
+ 
+    Student: Brett Jenaway
+    Student No: W0104215
+
+    Garden Indian Restaurant Website Design
+*/
+
